af129e6e71fd9057e370c2ec9e9cf5142181f205
Paul Schoenfelder <paulschoenfelder@gmail.com>
2014-05-07 01:25:53 -0500

    General refactoring. Added :inject and :only options to `use Protobuf`.

--- lib/exprotobuf/config.ex
+++ lib/exprotobuf/config.ex
@@ -1,8 +1,10 @@
-defmodule ExProtobuf.ConfigError do
-  defexception [:message]
+defexception Protobuf.ConfigError, error: nil do
+  def message(Protobuf.ConfigError[error: error]) do
+    inspect(error)
+  end
 end
 
-defmodule ExProtobuf.Config do
+defmodule Protobuf.Config do
   @moduledoc """
   Defines a struct used for configuring the parser behavior.
 
@@ -11,20 +13,10 @@ defmodule ExProtobuf.Config do
             only: [],       # The list of types to load, if empty, all are loaded
             inject: false   # Flag which determines whether the types loaded are injected in the 'using' module.
                             # `inject: true` requires only with a single type defined, since no more than one struct
-                            # can be defined per-module.
+                            # can be defined per-module. 
   """
   defstruct namespace: nil,
             schema: "",
             only: [],
-            inject: false,
-            from_file: nil,
-            use_package_names: false,
-            doc: nil
-
-
-  def doc_quote(false) do
-    quote do: @moduledoc unquote(false)
-  end
-
-  def doc_quote(_), do: nil
+            inject: false
 end
--- lib/exprotobuf/parser.ex
+++ lib/exprotobuf/parser.ex
@@ -1,48 +1,20 @@
-defmodule ExProtobuf.Parser do
+defmodule Protobuf.Parser do
 
-  defmodule ParserError do
-    defexception [:message]
-  end
-
-  def parse_files!(files, options \\ []) do
-    Enum.reduce(files, [], fn(path, defs) ->
-      schema = File.read!(path)
-      new_defs = parse!(schema, options)
-      defs ++ new_defs
-    end) |> finalize!(options)
-  end
-
-  def parse_string!(string, options \\ []) do
-    parse!(string, options) |> finalize!(options)
-  end
-
-  defp finalize!(defs, options) do
-    case :gpb_parse.post_process_all_files(defs, options) do
-      {:ok, defs}     -> defs
-      {:error, error} ->
-        msg = case error do
-          [ref_to_undefined_msg_or_enum: {{root_path, field}, type}] ->
-            type_ref    = Enum.map(type, &Atom.to_string/1) |> Enum.join
-            invalid_ref = Enum.reverse([field|root_path]) |> Enum.map(&Atom.to_string/1) |> Enum.join
-            "Reference to undefined message or enum #{type_ref} at #{invalid_ref}"
-          _ when is_binary(error) ->
-            error
-          _ ->
-            Macro.to_string(error)
-        end
-        raise ParserError, message: msg
+  defexception ParserError, error: nil do
+    def message(ParserError[error: error]) do
+      inspect(error)
     end
   end
 
-  defp parse(defs, options) when is_list(defs) do
-    :gpb_parse.post_process_one_file(defs, options)
-  end
-  defp parse(string, options) when is_binary(string) do
+  def parse(msgs),                             do: parse(msgs, [])
+  def parse(defs, options) when is_list(defs), do: :gpb_parse.post_process(defs, options)
+  def parse(string, options) do
     case :gpb_scan.string('#{string}') do
       {:ok, tokens, _} ->
         lines = String.split(string, "\n", parts: :infinity) |> Enum.count
         case :gpb_parse.parse(tokens ++ [{:'$end', lines + 1}]) do
-          {:ok, defs} -> parse(defs, options)
+          {:ok, defs} ->
+            parse(defs, options)
           error ->
             error
         end
@@ -51,21 +23,10 @@ defmodule ExProtobuf.Parser do
     end
   end
 
-  defp parse!(string, options) do
+  def parse!(string, options \\ []) do
     case parse(string, options) do
       {:ok, defs}     -> defs
-      {:error, error} ->
-        msg = case error do
-          [ref_to_undefined_msg_or_enum: {{root_path, field}, type}] ->
-            type_ref    = Enum.map(type, &Atom.to_string/1) |> Enum.join
-            invalid_ref = Enum.reverse([field|root_path]) |> Enum.map(&Atom.to_string/1) |> Enum.join
-            "Reference to undefined message or enum #{type_ref} at #{invalid_ref}"
-          _ when is_binary(error) ->
-            error
-          _ ->
-            Macro.to_string(error)
-        end
-        raise ParserError, message: msg
+      {:error, error} -> raise ParserError, error: error
     end
   end
 end
