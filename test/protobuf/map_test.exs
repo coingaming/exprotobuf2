6bcda6c4db7db8ad0aec49b58649bf477a107a6d
Kien Nguyen Trung <trungkien2288@gmail.com>
2016-05-31 13:42:56 +0700

    Upgrade gdb to 3.21.3 to support map<_,>

--- test/protobuf/map_test.exs
+++ test/protobuf/map_test.exs
@@ -1,8 +1,8 @@
-defmodule ExProtobuf.Map.Test do
-  use ExProtobuf.Case
+defmodule Protobuf.Map.Test do
+  use Protobuf.Case
 
   defmodule Msgs do
-    use ExProtobuf, from: Path.expand("../proto/map.proto", __DIR__)
+    use Protobuf, from: Path.expand("../proto/map.proto", __DIR__)
   end
 
   @binary <<10, 16, 10, 4, 110, 97, 109, 101, 18, 8, 10, 6, 101, 108, 105, 120, 105, 114>>
@@ -19,11 +19,11 @@ defmodule ExProtobuf.Map.Test do
 
   test "can decode map" do
     entity = @binary |> Msgs.Entity.decode
-    assert %Msgs.Entity{
+    assert entity = %Msgs.Entity{
       properties: [
         {"name", %Msgs.Value{value: "elixir"}}
       ]
-    } = entity
+    }
   end
 end
 
