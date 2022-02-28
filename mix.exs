b4c1d1553173474026433dbcd5c77fe1ccc8150d
Reio Piller <reio@heathmont.net>
2022-02-28 13:35:24 +0200

    fix: read VERSION in compile time

--- mix.exs
+++ mix.exs
@@ -1,28 +1,35 @@
 defmodule ExProtobuf.Mixfile do
   use Mix.Project
-
+  
+  @version (case File.read("VERSION") do
+    {:ok, version} -> String.trim(version)
+    {:error, _} -> "0.0.0-development"
+  end)
+  
   def project do
-    [app: :exprotobuf,
-     version: version(),
-     elixir: "~> 1.2",
-     elixirc_paths: elixirc_paths(Mix.env),
-     description: description(),
-     package: package(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     consolidate_protocols: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :exprotobuf,
+      version: @version,
+      elixir: "~> 1.2",
+      elixirc_paths: elixirc_paths(Mix.env),
+      description: description(),
+      package: package(),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      consolidate_protocols: Mix.env == :prod,
+      deps: deps(),
+      # docs
+      name: "Bennu",
+      source_url: "https://github.com/coingaming/exprotobuf2",
+      homepage_url: "https://github.com/coingaming/exprotobuf2/tree/v#{@version}",
+      docs: [
+        source_ref: "v#{@version}"
+      ]
+    ]
   end
 
   def application do
     [applications: [:gpb]]
-  end
-
-  defp version do
-    case File.read("VERSION") do
-      {:ok, v} -> String.trim(v)
-      {:error, _} -> "0.0.0-development"
-    end
   end
 
   defp description do
@@ -37,7 +44,7 @@ defmodule ExProtobuf.Mixfile do
       files: ["lib", "mix.exs", "README.md", "LICENSE", "VERSION"],
       maintainers: ["Paul Schoenfelder", "Ilja Tkachuk aka timCF"],
       licenses: ["Apache Version 2.0"],
-      links: %{"GitHub": "https://github.com/coingaming/exprotobuf2"} ]
+      links: %{"GitHub": "https://github.com/coingaming/exprotobuf2/tree/v#{@version}"} ]
   end
 
   defp deps do
